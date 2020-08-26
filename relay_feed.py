b0e9c0be7dcc949820176ec184bd56bf4d3bf1b9
RPi#1 <aylinmmcgough@gmail.com>
2020-08-26 11:41:08 -0600

    Updated for PWM functionality rather than manual toggle. Receives pin # and duty cycle from TCS_GUI.py in order to update GPIO.PWM method for individual relays.

--- relay_feed.py
+++ relay_feed.py
@@ -6,16 +6,22 @@ import sys,os
 
 ser=serial.Serial(port='/dev/serial0',baudrate=115200, bytesize=serial.EIGHTBITS, timeout=0)
 
-
-relay = {7:38,8:11,9:12,10:13,11:15,12:16,13:18,14:22,15:29,16:31,17:32,
-	18:33,19:36,20:37,27:35,28:40}
+#relay#:[GPIO,index]
+relay = {7:[38,0],8:[11,1],9:[12,2],10:[13,3],11:[15,4],12:[16,5],13:[18,6],
+	14:[22,7],15:[29,8],16:[31,9],17:[32,10],18:[33,11],19:[36,12],
+	20:[37,13],27:[35,14],28:[40,15]}
 
 GPIO.setmode(GPIO.BOARD)
 
+pwm = []
 for key, value in relay.items():
-	GPIO.setup(value,GPIO.OUT)
-	GPIO.output(value,GPIO.HIGH)
-	print "%s pin setup" %value
+	GPIO.setup(value[0],GPIO.OUT)
+	GPIO.output(value[0],GPIO.HIGH)
+	pwm.append(GPIO.PWM(value[0],0.5))
+	print "%s pin setup" %value[0]
+for x in range(len(pwm)):
+	#print x
+	pwm[x].start(100)
 
 dat=''
 def main():
@@ -24,19 +30,21 @@ def main():
 		time.sleep(0.1)	
 		r=ser.inWaiting()
 		pin += ser.read(r)
-		
-		if pin:
-			pin=int(pin)
-			#print pin
-			#time.sleep(0.1)
 			
-			if pin in relay:
-				GPIO.output(relay[pin],GPIO.LOW)
-			elif pin/100 in relay:
-				GPIO.output(relay[pin/100],GPIO.HIGH)
-			else:
-				pass
-			pass
+		if pin:
+			back = pin.split()
+			print pin
+				
+			if pin == '1111 1':
+				for x in range(len(pwm)):
+					pwm[x].stop()
+				GPIO.cleanup()
+				sys.exit()
+			
+			elif int(back[0]) in relay:
+				loop = relay[int(back[0])][1]
+				control = int(back[1])
+				pwm[loop].ChangeDutyCycle(100-control)	
 		else:
 			pass
 
