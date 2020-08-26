427b8b7e12854e7dc08e90d20a70a0896c7c5321
RPi#1 <aylinmmcgough@gmail.com>
2020-08-26 11:38:47 -0600

    Added PWM and PID loops for GPIO relays. Need to create PWM method for relays attached to LP boards and add options to change PID coefficients. Removed heater/relay manual toggle.

--- TCS_GUI.py
+++ TCS_GUI.py
@@ -9,6 +9,8 @@ from tcs import Ui_TCS
 import TEMP as temp #HP sensor routine
 import low_p_temp_boards as lowp
 from pexpect import pxssh
+import numpy as np
+import PID
 
 ser=serial.Serial(port='/dev/serial0',baudrate=115200,bytesize=serial.EIGHTBITS,timeout=0)
 
@@ -18,7 +20,6 @@ relay={1:29,2:31,3:32,4:33,5:36,6:37,7:38,8:11,9:12,10:13,11:15,12:16,13:18,14:2
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-
 
 lnk = pxssh.pxssh()
 hn = '10.212.212.70'
@@ -36,9 +37,26 @@ def GPIO_setup(pin):
 	print "%s pin setup" %pin
 
 def blockPrint():
-	sys.stdout=open(os.devnull,'w')
+	sys.stdout = open(os.devnull,'w')
+def enablePrint():
+	sys.stdout = sys.__stdout__
 
-#blockPrint()
+#Update parameters according to the configuration file (option to change PID parameters w/out restarting script):
+def readConfig():
+	global targetT
+	with open ('/tmp/pid.conf','r') as f:
+		config=f.readline().split(',')
+		pid.SetPoint=float(config[0]) #capability to change target temp.
+		targetT=pid.SetPoint
+		pid.setKp=float(config[1])
+		pid.setKi=float(config[2])
+		pid.setKd=float(config[3])
+
+def createConfig():
+	if not os.path.isfile('/tmp/pid.conf'):
+		with open ('/tmp/pid.conf','w') as f:
+			f.write('%s,%s,%s,%s' %(targetT,P,I,D))
+
 hp = temp.TEMP() #TEMP class of HP sensors 
 lp = lowp.LP() #LP class for LP boards
 
@@ -110,6 +128,36 @@ class MainWindow(QtWidgets.QMainWindow, Ui_TCS):
 		self.pb_h11_top.clicked.connect(lambda:self.relay(self.pb_h11_top,27))
 		self.pb_h12_top.clicked.connect(lambda:self.relay(self.pb_h12_top,28))
 
+		self.PWM_setup()
+		self.PID_setup()
+
+	def PWM_setup(self):
+		self.pwm = []
+		for x in range(28):
+			x +=1
+			if x <= 5 or x == 7:
+				self.pwm.append(GPIO.PWM(relay[x],0.5))
+				if x == 7:
+					x = 6
+				self.pwm[x-1].start(100)
+			elif 8 <= x <= 20 or x in [6,27,28]:
+				pass #PWM setup in relay_feed.py
+			elif 21 <= x <= 26:
+				print 'Work in progress XP' 
+
+	def PID_setup(self):
+		targetT = 32
+		P = 10
+		I = 1
+		D = 1
+
+		self.pid = []
+		for x in range(28):
+			self.pid.append(PID.PID(P,I,D))
+			self.pid[x].SetPoint = targetT
+			self.pid[x].setSampleTime(1)
+
+	'''
 	def relay(self, btn, ind):
 		if ind < 16:
 			ind1="btm: "+str(ind)
@@ -117,21 +165,23 @@ class MainWindow(QtWidgets.QMainWindow, Ui_TCS):
 			ind1="top: "+str(ind-16)
 
 		if btn.isChecked():
-			print 'checked'
 			if ind <= 6:
 				GPIO.output(relay[ind],GPIO.LOW)
 			elif 7 <= ind <= 20 or ind == 27 or ind == 28:
+				blockPrint()
 				ser.write(str(ind).strip('\r\n'))
+				enablePrint()
 			elif 21 <= ind <= 26:	
 				lp.Relay_ON(ind)
 
 			print "Relay "+ind1+" ON"
 		else:
-			print 'not checked'
 			if ind <= 6:
 				GPIO.output(relay[ind],GPIO.HIGH)
 			elif 7 <= ind <= 20 or ind == 27 or ind == 28:
+				blockPrint()
 				ser.write(str(ind*100).strip('\r\n'))
+				enablePrint()
 			elif 21 <= ind <= 26:	
 				lp.Relay_OFF(ind)
 	
@@ -147,12 +197,14 @@ class MainWindow(QtWidgets.QMainWindow, Ui_TCS):
 		if ind <= 6:
 			GPIO.output(relay[ind],GPIO.HIGH)
 		elif 7 <= ind <= 20 or ind == 27 or ind == 28:
+			blockPrint()
 			ser.write(str(ind*100).strip('\r\n'))
+			enablePrint()
 		elif 21 <= ind <= 26:	
 			lp.Relay_OFF(ind)
 	
 		print "Relay "+ind1+" OFF"
-		
+	'''
 
 	def hp_update(self, data):
 		objects = {
@@ -243,12 +295,49 @@ class MainWindow(QtWidgets.QMainWindow, Ui_TCS):
 			#59: ,
 			#60: }
 
+		#LP diodes matched with heating pads and PID loops
+		LPHeating = {0:[3,0],2:[6,1],4:[9,2],6:[12,3],7:[2,4],9:[5,5],11:[8,6],
+			13:[11,7],14:[1,8],16:[4,9],18:[7,10],20:[10,11],21:[14,12],
+			23:[17,13],25:[20,14],30:[23,15],32:[26,16],33:[15,17],35:[18,18],
+			37:[21,19],39:[24,20],40:[16,21],42:[19,22],44:[22,23],46:[25,24],
+			48:[27,25],50:[13,26],53:[28,27]}
+
+		relay = {1:self.lb_h1_top,2:self.lb_h2_top,3:self.lb_h3_top,4:self.lb_h4_top,
+			5:self.lb_h5_top,6:self.lb_h6_top,7:self.lb_h7_top,8:self.lb_h8_top,
+			9:self.lb_h9_top,10:self.lb_h10_top,11:self.lb_h11_top,12:self.lb_h12_top,
+			13:self.lb_h1,14:self.lb_h2,15:self.lb_h3,16:self.lb_h4,17:self.lb_h5,
+			18:self.lb_h6,19:self.lb_h7,20:self.lb_h8,21:self.lb_h9,22:self.lb_h10,
+			23:self.lb_h11,24:self.lb_h12,25:self.lb_h13,26:self.lb_h14,27:self.lb_h15,
+			28:self.lb_h16}
+
+		#if data[0] in objects.keys() and objects[data[0]].isChecked() == True:
 		if data[0] in objects.keys():
 			pre = objects[data[0]].text()
 			pre = pre.split(':')[0]
 			time.sleep(0.01)
-			objects[data[0]].setText(pre+": %.2f C" %data[1])	
+			if data[1] == 11111:
+				objects[data[0]].setText("N/C")
+			else:
+				objects[data[0]].setText(pre+": %.2f C" %data[1])
 
+		if data[0] in LPHeating.keys():
+			loop = LPHeating[data[0]][1]
+			heat = LPHeating[data[0]][0]
+			self.pid[loop].update(data[1])
+			control = self.pid[loop].output
+			control = max(min(int(control),100),0)
+			relay[heat].setText(str(control)+chr(37))
+
+			if 8 <= heat <= 20 or heat in [6,27,28]:
+				blockPrint()
+				ser.write(str(heat)+" "+str(control))
+				enablePrint()
+			elif heat <= 5 or heat == 7:
+				self.pwm[loop].ChangeDutyCycle(100-control)
+			print loop
+			
+			
+		'''
 		relay = {0:[self.pb_h5_top,self.pb_h6_top],1:[self.pb_h7_top,self.pb_h8_top],
 			2:[self.pb_h9_top,self.pb_h10_top]}
 		
@@ -256,7 +345,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_TCS):
 		ind = -1
 		for x in Rstatus:
 			ind += 1
-
+			
+			#print x
 			if 0x330000 & x == 0x330000: #both off
 				relay[ind][0].setChecked(False)
 				relay[ind][1].setChecked(False)
@@ -272,6 +362,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_TCS):
 			else: #both on
 				relay[ind][0].setChecked(True)
 				relay[ind][1].setChecked(True)
+		'''
 
 	def closeEvent(self,event):
 		reply = QtWidgets.QMessageBox.question(self,'Window Close', 'Are you sure you want to close the window?',
@@ -280,7 +371,14 @@ class MainWindow(QtWidgets.QMainWindow, Ui_TCS):
 			self.lpthread.stop(); self.hpthread.stop()
 			for x in range(28):
 				x += 1
-				self.relay_off(x)
+				if x <= 6:
+					self.pwm[x-1].stop()
+				elif x == 7:
+					blockPrint()
+					ser.write('1111 1')
+					enablePrint()
+				#self.relay_off(x)
+			GPIO.cleanup()
 			event.accept()
 			print 'Window Closed'
 		else:
@@ -310,6 +408,15 @@ class LPSensorThread(QThread):
 		QThread.__init__(self)
 
 	def run(self):
+		for s in range(61): #9 LP outside (sensor#27-29,56-61)
+			if s in [26,27,28,55,56,57,58,59,60]:
+				pass
+			elif 0 <= s <= 20:
+				pass
+			else:
+				lpboard = lp.getTemp(s)
+				time.sleep(0.01)
+
 		while True:
 			for s in range(61): #9 LP outside (sensor#27-29,56-61)
 				if s in [26,27,28,55,56,57,58,59,60]:
@@ -317,14 +424,20 @@ class LPSensorThread(QThread):
 				elif 0 <= s <= 20:
 					pass
 				else:
-					lpboard = lp.getTemp(s)
-					lptemp = lpboard[0]
+					lptemp = []
+					for x in range(10):
+						lpboard = lp.getTemp(s)
+						lptemp.append(lpboard[0])
+						time.sleep(0.01)
 					Rstatus = lpboard[1]
+					lptemp = np.average(lptemp)
+					print 'Sensor %s: %.5s' %(s,lptemp)
 					self.signal.emit([s,lptemp,Rstatus])
-					time.sleep(1)	
+					time.sleep(0.5)	
 	def stop(self):
 		self.terminate()
 
+'''
 class RelayThread(QThread):
 	signal = pyqtSignal('PyQt_PyObject')
 
@@ -334,6 +447,7 @@ class RelayThread(QThread):
 	def run(self):
 		while True:
 			Rstatus = lp.getTemp(s)[1]
+'''
 					
 
 #PID loop will operate automatically and toggle relays. For now, just have the functionallity to manually toggle relays?
