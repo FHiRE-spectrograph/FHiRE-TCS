1bc9898aa26d404802ca8593d78aef06c60f21f4
RPi#1 <aylinmmcgough@gmail.com>
2020-09-16 15:55:22 -0600

    Cleaned up code and added better comments.

--- relay_feed.py
+++ relay_feed.py
@@ -1,5 +1,3 @@
-## Paired with temp_sensor.py on other RPI. 
-
 import serial, time
 import RPi.GPIO as GPIO
 import sys,os
@@ -13,19 +11,19 @@ relay = {7:[38,0],8:[11,1],9:[12,2],10:[13,3],11:[15,4],12:[16,5],13:[18,6],
 
 GPIO.setmode(GPIO.BOARD)
 
+#Setup PWM for RPi2 relays
 pwm = []
 for key, value in relay.items():
 	GPIO.setup(value[0],GPIO.OUT)
 	GPIO.output(value[0],GPIO.HIGH)
 	pwm.append(GPIO.PWM(value[0],0.5))
-	print "%s pin setup" %value[0]
+
 for x in range(len(pwm)):
-	#print x
 	pwm[x].start(100)
 
-dat=''
 def main():
 	while True:
+		#Read message from RPi1 via UART comm.
 		pin=ser.read()
 		time.sleep(0.1)	
 		r=ser.inWaiting()
@@ -33,14 +31,15 @@ def main():
 			
 		if pin:
 			back = pin.split()
-			print pin
-				
+			
+			#Stop all PWM comm. and cleanup GPIO pins - exit
 			if pin == '1111 1':
 				for x in range(len(pwm)):
 					pwm[x].stop()
 				GPIO.cleanup()
 				sys.exit()
 			
+			#Update PWM with new duty cycle
 			elif int(back[0]) in relay:
 				loop = relay[int(back[0])][1]
 				control = int(back[1])
@@ -49,14 +48,5 @@ def main():
 			pass
 
 if __name__=='__main__':
-	try:
-		main()
-	except KeyboardInterrupt:
-		print("*Interrupted*")
-		GPIO.cleanup()
-		ser.close()
-		try:
-			sys.exit(0)
-		except SystemExit:
-			os._exit(0)
+	main()
 
