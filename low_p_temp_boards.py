1a0a72abe389589c13fa8b83778ef9f8df2544a4
RPi#1 <aylinmmcgough@gmail.com>
2021-02-16 23:45:39 -0700

    Overhauled the coding. Based organization on public C script for converting with an AD7124-8 ADC. Includes new wiring designations for sensors. Sensors still read different values depending on their pin connection. Need to measure a baseline voltage and the excitation current?

--- low_p_temp_boards.py
+++ low_p_temp_boards.py
@@ -9,131 +9,181 @@ spi.open(0,0)
 spi.mode = 3
 spi.no_cs=False #set false or true? false = manually control spi devices cs. error. Doesn't seem to do anything.
 
-#Convert decimal/hex to a byte array
-def bytes(decimal,bitsize):
-	if bitsize == 8:
-		x=decimal
- 		return [int(x)]
-	elif bitsize == 16:
-		y=math.floor(decimal*2**(-8))
-		x=decimal-y*2**8
-		return [int(y),int(x)]
-	elif bitsize == 24:
-		z=math.floor(decimal*2**(-16))
-		d2=decimal-z*2**16
-		y=math.floor(d2*2**(-8))
-		x=d2-y*2**8
-		return [int(z),int(y),int(x)]
-
-
-#Convert byte array to a decimal
-def decimal(b):
-	bitsize=len(b)
-	if bitsize == 1: #8bit
-		decimal=b[0]
-	elif bitsize == 2: #16bit
-		decimal=b[1]+(b[0]<<8)
-	elif bitsize == 3: #24bit
-		decimal=b[2]+(b[1]<<8)+(b[0]<<16)
-	elif bitsize == 4:
-		decimal=b[3]+(b[2]<<8)+(b[1]<<16)+(b[0]<<32)
-
-	return decimal
-
-
 class LP:
 	def __init__(self):
 		GPIO.setmode(GPIO.BOARD)
-		#GPIO.cleanup()
-		GPIO_pins=[11,12,13,15,16,18,22,38] #CS 2-8, 1
+
+		#GPIO_pins=[11,12,13,15,16,18] #CS 1-7
+		GPIO_pins = [11,12]
+
+		#Relay:[CS GPIO - board,pin]
+		#relay2={21:[38,1],22:[38,2],23:[11,1],24:[11,2],25:[12,1],26:[12,2]}
 
 		#Setup all CS pins and hold high
 		for x in GPIO_pins:
 			GPIO.setup(x,GPIO.OUT)
 			GPIO.output(x,GPIO.HIGH)
 
-		global read, write, reset, state, relay2
-
-		read = 0x40
-		write = 0x00
-
-		#Relay:[CS GPIO - board,pin]
-		relay2={21:[38,1],22:[38,2],23:[11,1],24:[11,2],25:[12,1],26:[12,2]}
-
-		#Setup relay boards
 		for x in GPIO_pins:
+			self.Initialize(x)
 			self.Setup(x)
 
-		print "Setup complete"
+		#self.Initialize(11)
+		#self.Setup(11)
 
-	def commands(self, board, channel, rw, cmd, bits):
-		
+		#self.Initialize(12)
+		#self.Setup(12)
+
+		print "AD7124-8 setup complete"
+
+	def Initialize(self, board):
+		print("Initializing")
+
+		#Reset ADC
+		reset = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF] #Reset w/ 64 consecutive 1s
 		GPIO.output(board,GPIO.LOW)
-		if rw == 'w': 
-			spi.writebytes(bytes(write|channel,8))
-			spi.writebytes(bytes(cmd,bits))
-		elif rw == 'r':	
-			spi.writebytes(bytes(read|channel,8))
-			return decimal(spi.readbytes(bits/8))
-		time.sleep(0.05)
+		spi.writebytes(reset)
+		time.sleep(0.1) #Could rest for 1ms?
 		GPIO.output(board,GPIO.HIGH)
-		
+
+		#Check ID register
+		ID = self.commands(board,[0x45])
+		if ID[0] != 0x14:
+			print("** ADC reset failed (board GPIO %s)- bad ID: %s " %(board,ID))
+
+		#Read status register twice
+		self.commands(board,[0x40])
+		status = self.commands(board,[0x40])
+		if status[0] != 0x80:
+			print("** ADC reset failed (board GPIO %s)- bad Status: %s " %(board,status))
+
+		print("Initialization complete (board GPIO %s)" %board)
 
 	def Setup(self, board):
-		'''
-		#Write to CONFIG_0 register: Unipolar, ext. volt=ref, gain=1
-		GPIO.output(board,GPIO.LOW)
-		spi.writebytes(bytes(write|0x19,8)) 
-		#spi.writebytes(bytes(0x18,16)) #AVDD=ref
-		spi.writebytes(bytes(0x0,16)) #Ext. Volt=ref
+		self.commands(board,[0x19,0x00,0x00]) #Setup config_0 - unipolar, ref=ext.ref1, no buffers, PGA=0
+		#self.commands(board,[0x19,0x00,0x18])
+		self.commands(board,[0x21,0x00,0x60,0x18]) #Setup filter_0 - default
+		#self.commands(board,[0x01,0x00,0x08]) #Setup ADC_CONTROL - standby mode
+		self.commands(board,[0x07,0x03,0xF0,0x38]) #Setup error register 
+
+	def getTemp(self,sensor):
+		sensor += 1
+		#sensor = 19
+
+		s = {1:[11,0,None,'1-1'],2:[11,2,None,'1-2'],3:[11,2,None,'1-3'],4:[11,3,None,'1-4']
+		,5:[11,4,None,'1-5'],6:[11,6,None,'1-6'],7:[11,7,None,'1-7'],8:[11,8,None,'1-8']
+		,9:[11,9,None,'1-9'],10:[11,10,None,'1-10'],11:[11,11,None,'1-11'],12:[11,12,None,'1-12']
+		,13:[11,13,None,'1-13'],14:[11,14,None,'1-14'],15:[12,1,None,'2-1'],16:[12,2,None,'2-2']
+		,17:[12,6,None,'2-3'],18:[12,7,None,'2-4'],19:[12,8,None,'2-5'],20:[12,9,None,'2-6']
+		,21:[12,10,None,'2-7']}
+
+		diodes = [1,3,5,7,8,10,12,14,15,17,19,21]
+		resistors = [2,4,6,9,11,13,16,18,20]
+
+		board = s[sensor][0]; pin1 = s[sensor][1]; pin2 = pin1 + 1
+
+		#conf = IOUT, conf2 = Vin
+		conf = {0:0x0,1:0x1,2:0x2,3:0x3,4:0x4,5:0x5,6:0x6,7:0x7,8:0x8,9:0x9,
+			10:0xA,11:0xB,12:0xC,13:0xD,14:0xE,15:0xF}
+
+		Iout = conf[pin1]; Vin = conf[pin1]
+
+		if sensor in [3,4]:
+			return 0
+		elif sensor in [1,2]:
+			Iout = conf[pin2]
+		elif sensor in [16,17,18,19]:
+			Iout = 0x0
+			#self.commands(board,[0x03,0x00,0x06,0x0])
+			mult = 1
+
+
+		self.commands(board,[0x09,0x80|(Vin>>3),0x05|(Vin<<5 & 0xFF)]) #Setup channel_0 - AINP=pin1, AINM=AIN5
+
+		self.commands(board,[0x03,0x00,0x04,Iout]) #Setup Iout - Iout0 500microA on pin1
+
+		self.commands(board,[0x01,0x02,0x04]) #ADC_CONTROL - update to single conversion - begins conversion
+
+		print("Sensor %s --------" %sensor)
+		while True:
+			time.sleep(0.1)
+			status = self.commands(board,[0x40])
+			print("Status: %s" %status)
+			if status[0]&0x40 == 0x40:
+				print("LP ADC ERROR - checking error register")
+				print("Error: %s" %self.commands(board,[0x46]))
+
+			elif status[0]&0x80 != 0x80:
+				time.sleep(0.01)
+				data = self.commands(board,[0x42])
+				print("Data: %s" %data)
+				if data[0] | data[1] | data[2] == 0:
+					print("***** ZERO VALUE ERROR *****")
+				elif data == [0xFF,0xFF,0xFF]:
+					print("***** Sensor N/C ERROR *****")
+				break
+
+		self.commands(board,[0x03,0x00,0x00,0x00]) #Turn off Iout - 500 microA AIN3
+
+		data = data[2]+(data[1]<<8)+(data[0]<<16)
+
+		if sensor in resistors:
+			r = [31.3231717,2.20213448,1.66482204e-3,-3.98620116e-6,4.74888650e-9,
+				7.27263261e-12,-2.80325700e-14,3.11300149e-17,-1.22123964e-20] 
+
+			R = data*2.5/(2**(24)*500e-6)
+			V = data*2.5/(2**(24))
+			print("Voltage: %.7s V" %V)
+			print("Resistance: %.7s Ohms" %R)
+
+			polyR = np.polynomial.Chebyshev(r)
+			output = polyR(R)-273.15
+			print("Temperature: %.5s C" %(polyR(R)-273.15))
+			#output = V
+
+		elif sensor in diodes:	
+			d = [2.06215201e4,-3.72240694e4,2.81733106e4,-1.78708069e4,9.20412215e3,
+				-3.73253855e3,1.12440290e3,-2.25464342e2,22.4784626]
+			#d = [5.50407261e2, -6.96091175e-3, 1.11679275e-7, -2.36461493e-12, 2.90727397e-17, 					-2.15428815e-22, 9.44575204e-28, -2.25464342e-33, 2.24784626e-39]
+
+			R = data*2.5/(2**(24)*500e-6)
+			V = data*2.5/(2**(24))
+			print("Voltage: %.7s V" %V)
+			print("Resistance: %.7s Ohms" %R)
+
+			polyD = np.polynomial.Chebyshev(d)
+			output = polyD(V)-273.15
+			print("Temperature: %.5s C" %(polyD(V)-273.15))
+			#output = V
+
+		error = self.commands(board,[0x46])
+		if error[0] | error[1] | error[2] == 0:
+			#print("No errors - keep data")
+			return output
+		else:
+			print("Error - throwing out data")
+			print("Error: %s" %error)
+			return 11111
+
+	#Send a command to AD7124-8 boards - board/CS, byte command
+	def commands(self, board, cmd):
 		time.sleep(0.05)
-		GPIO.output(board,GPIO.HIGH)
-
-		#Write to FILTER_0 register: default
 		GPIO.output(board,GPIO.LOW)
-		spi.writebytes(bytes(write|0x21,8)) 
-		spi.writebytes(bytes(0x06018,24)) 
-		time.sleep(0.05)
-		GPIO.output(board,GPIO.HIGH)	
+		spi.writebytes(cmd)
+		
+		if cmd[0] & 0x40 == 0x40:
+			data = spi.readbytes(3)
+			GPIO.output(board,GPIO.HIGH)
+			return data
+		else:
+			GPIO.output(board,GPIO.HIGH)
 
-		#Write to ADC_CONTROL register: disable int. voltage, low-power, continuous conversion mode, int. clock source
-		GPIO.output(board,GPIO.LOW)
-		spi.writebytes(bytes(write|0x01,8))
-		spi.writebytes(bytes(0x0004,16)) 
-		time.sleep(0.05)
-		GPIO.output(board,GPIO.HIGH)
+#======================#
+#    Relay toggle
+#======================#
 
-		#Write to CHANNEL_0 register: conversion=disabled, setup_0, AIN0=pos, AIN0=neg
-		GPIO.output(board,GPIO.LOW)
-		spi.writebytes(bytes(write|0x09,8))
-		spi.writebytes(bytes(0x8033,16))
-		time.sleep(0.05)
-		GPIO.output(board,GPIO.HIGH)
-
-		#Write to ERROR_EN register
-		GPIO.output(board,GPIO.LOW)
-		spi.writebytes(bytes(write|0x07,8))
-		spi.writebytes(bytes(0x20000,24))
-		time.sleep(0.05)
-		GPIO.output(board,GPIO.HIGH)
-		'''
-
-		self.commands(board,0x19,'w',0x0,16) #Write to CONFIG_0 register: Unipolar, ext.volt=ref, gain=1
-		self.commands(board,0x21,'w',0x06018,24) #Write to FILTER_0 register: default
-		self.commands(board,0x01,'w',0x0004,16) #Write to ADC_CONTROL register: disable int.volt, low-power, continuous conversion mode, int. clock source
-		self.commands(board,0x09,'w',0x8033,16) #Write to CHANNEL_0 register: conversion=disabled, setup_0, AIN0=pos, AIN0=neg
-		self.commands(board,0x07,'w',0x20000,24) #Write to ERROR_EN register: enable conversion error
-
-
-#########################################################
-# Relay Toggle Methods
-#########################################################
 	def check_status(self,board,pin):
-		status = self.commands(board,0x03,'r',0x0,24)
-		#GPIO.output(board,GPIO.LOW)
-		#spi.writebytes(bytes(read|0x03,8))
-		#status = decimal(spi.readbytes(3))
-		#GPIO.output(board,GPIO.HIGH)
+		status = self.commands(board,[0x43])
 
 		if pin == 1: #Check if pin 2 is ON/OFF
 			if status & 0x220000 == 0x220000: #OFF
@@ -166,11 +216,8 @@ class LP:
 		
 		S = self.check_status(board,pin)
 
-		#Write IO_Control_1 register: pin=low	
-		GPIO.output(board,GPIO.LOW)
-		spi.writebytes(bytes(write|0x03,8)) 
-		spi.writebytes(bytes(P|S,24)) 
-		GPIO.output(board,GPIO.HIGH)
+		#self.commands(board,0x03,'w',P|S,24) #Write IO_Control_1 register: pin=low	
+		self.commands(board,[0x03,P|S]) #might work 
 
 	def Relay_OFF(self, ind): 
 		board = relay2[ind][0]
@@ -185,183 +232,9 @@ class LP:
 
 		S = self.check_status(board,pin)
 
-		#Read IO_Control_1 register: pin=high
-		GPIO.output(board,GPIO.LOW)
-		spi.writebytes(bytes(write|0x03,8)) 
-		spi.writebytes(bytes(P|S,24)) 
-		GPIO.output(board,GPIO.HIGH)
+		#self.commands(board,0x03,'w',P|S,24) #Write IO_Control_1 register: pin=high
+		self.commands(board,[0x03,P|S]) #might work 
 
 
-###########################################################
-# Read Temperature Method
-###########################################################
 
-	def getTemp(self, sensor):
-		sensor += 1
-
-		#sensor:[CS GPIO - board,first AIN pin#,resistance at room temp] 29,28,27,61-56 = outside
-		#s = {1:[38,0,0.5668,'1-1'],2:[38,4,41.4,'1-2'],3:[38,6,0.5662,'1-3'],4:[38,8,0.5431,'1-4'],5:[38,10,107.58,'1-5'],6:[38,12,0.5431,'1-6'],7:[38,14,0.5433,'1-7'],
-		#	8:[11,0,0.5679,'2-1'],9:[11,4,108.75,'2-2'],10:[11,6,0.5436,'2-3'],11:[11,8,108.85,'2-4'],12:[11,10,0.565,'2-5'],13:[11,12,108.75,'2-6'],14:[11,14,0.5447,'2-7'],
-		#	15:[12,0,0.5443,'3-1'],16:[12,4,109.98,'3-2'],17:[12,6,0.5666,'3-3'],18:[12,8,110.35,'3-4'],19:[12,10,0.5447,'3-5'],20:[12,12,110.0,'3-6'],21:[12,14,0.5684,'3-7'],
-		#	22:[13,0,0.5681,'4-1'],23:[13,2,110.85,'4-2'],24:[13,4,0.5439,'4-3'],25:[13,6,110.84,'4-4'],26:[13,8,0.5435,'4-5'],27:[13,10],28:[13,12],29:[13,14],
-		#	30:[15,0,111.5,'5-1'],31:[15,2,0.5438,'5-2'],32:[15,4,113.7,'5-3'],33:[15,6,0.5438,'5-4'],34:[15,8,0.5446,'5-5'],35:[15,10,114.8,'5-6'],36:[15,12,0.5441,'5-7'],37:[15,14,113.6,'5-8'],
-		#	38:[16,0,0.5441,'6-1'],39:[16,2,116.3,'6-2'],40:[16,4,0.5689,'6-3'],41:[16,6,0.5442,'6-4'],42:[16,8,113.9,'6-5'],43:[16,10,0.5444,'6-6'],44:[16,12,114.6,'6-7'],45:[16,14,0.5451,'6-8'],
-		#	46:[18,0,112.4,'7-1'],47:[18,2,0.5435,'7-2'],48:[18,4,113.4,'7-3'],49:[18,6,0.5433,'7-4'],50:[18,8,112.4,'7-5'],51:[18,10,0.5675,'7-6'],52:[18,12,112.4,'7-7'],53:[18,14,110.7,'7-8'],
-		#	54:[22,0,0.5686,'8-1'],55:[22,2,109.68,'8-2'],56:[22,4,1,'8-3'],57:[22,6,0.565,'8-4'],58:[22,8,110,'8-5'],59:[22,10,110,'8-6'],60:[22,12,110,'8-7'],61:[22,14,1,'8-8']}
-
-		s = {1:[38,0,0.57402,'1-1'],2:[38,4,50539,'1-2'],3:[38,6,0.5733,'1-3'],4:[38,8,0.5486,'1-4'],5:[38,10,107.12,'1-5'],6:[38,12,0.5488,'1-6'],7:[38,14,0.5495,'1-7'],
-			8:[11,0,0.575,'2-1'],9:[11,4,109.02,'2-2'],10:[11,6,0.55,'2-3'],11:[11,8,108.41,'2-4'],12:[11,10,0.5749,'2-5'],13:[11,12,109.939,'2-6'],14:[11,14,0.5497,'2-7'],
-			15:[12,0,0.5503,'3-1'],16:[12,4,110.935,'3-2'],17:[12,6,0.5734,'3-3'],18:[12,8,112.391,'3-4'],19:[12,10,0.5506,'3-5'],20:[12,12,110.367,'3-6'],21:[12,14,0.5751,'3-7'],
-			22:[13,0,0.5754,'4-1'],23:[13,2,111.716,'4-2'],24:[13,4,0.5509,'4-3'],25:[13,6,111.710,'4-4'],26:[13,8,0.5505,'4-5'],27:[13,10],28:[13,12],29:[13,14],
-			30:[15,0,113.096,'5-1'],31:[15,2,0.5509,'5-2'],32:[15,4,114.821,'5-3'],33:[15,6,0.5509,'5-4'],34:[15,8,0.5521,'5-5'],35:[15,10,117.85,'5-6'],36:[15,12,0.5516,'5-7'],37:[15,14,109.8,'5-8'],
-			38:[16,0,0.5513,'6-1'],39:[16,2,117.78,'6-2'],40:[16,4,0.5763,'6-3'],41:[16,6,0.5515,'6-4'],42:[16,8,114.319,'6-5'],43:[16,10,0.552,'6-6'],44:[16,12,113.995,'6-7'],45:[16,14,0.5526,'6-8'],
-			46:[18,0,112.067,'7-1'],47:[18,2,0.5515,'7-2'],48:[18,4,114.547,'7-3'],49:[18,6,0.5512,'7-4'],50:[18,8,114.54,'7-5'],51:[18,10,0.5755,'7-6'],52:[18,12,110.27,'7-7'],53:[18,14,110.852,'7-8'],
-			54:[22,0,0.5756,'8-1'],55:[22,2,110.643,'8-2'],56:[22,4,1,'8-3'],57:[22,6,0.565,'8-4'],58:[22,8,110,'8-5'],59:[22,10,110,'8-6'],60:[22,12,110,'8-7'],61:[22,14,1,'8-8']}
-
-		diodes = [1,3,4,5,7,8,10,12,14,15,17,19,21,22,24,
-			26,31,33,34,36,38,40,41,43,45,47,49,51,54,57]
-		resistors = [2,6,9,11,13,16,18,20,23,25,30,
-			32,35,37,39,42,44,46,48,50,52,53,55,58,59,60]
-
-		board = s[sensor][0]; pin1 = s[sensor][1]; pin2 = pin1 + 1
-
-		#conf = IOUT, conf2 = Vin
-		conf = {0:0x0,1:0x1,2:0x2,3:0x3,4:0x4,5:0x5,6:0x6,7:0x7,8:0x8,9:0x9,
-			10:0xA,11:0xB,12:0xC,13:0xD,14:0xE,15:0xF}
-		conf2 = {0:0x0,1:0x20,2:0x40,3:0x60,4:0x80,5:0xA0,6:0xC0,7:0xE0,8:0x100,
-			9:0x120,10:0x140,11:0x160,12:0x180,13:0x1A0,14:0x1C0,15:0x1E0}
-
-		Iout = conf[pin1]; Vin = conf2[pin2]
-
-		Channel_enable = 0x8013|Vin
-		IO_control = 0x100|Iout #50 microA set on AIN pin2
-
-		#Write to Channel_0 register: Update positive AIN for sensor
-		GPIO.output(board,GPIO.LOW)
-		spi.writebytes(bytes(write|0x09,8))
-		spi.writebytes(bytes(Channel_enable,16))
-		time.sleep(0.05)
-		GPIO.output(board,GPIO.HIGH)
-		time.sleep(0.05)
-
-		#Write IO_Control_1 register: Update Iout AIN output	
-		GPIO.output(board,GPIO.LOW)
-		spi.writebytes(bytes(write|0x03,8)) 
-		spi.writebytes(bytes(IO_control,24))
-		time.sleep(0.05)
-		GPIO.output(board,GPIO.HIGH)	
-		time.sleep(0.05)
-
-		#Write to ADC_CONTROL register: Update power mode to single conversion - begins conversion 
-		GPIO.output(board,GPIO.LOW)
-		spi.writebytes(bytes(write|0x01,8))
-		spi.writebytes(bytes(0x0004,16)) 
-		time.sleep(0.05)
-		GPIO.output(board,GPIO.HIGH)
-		time.sleep(0.05)
-
-		#GPIO.output(board,GPIO.LOW)
-		#spi.writebytes(bytes(read|0x07,24))
-		#print(decimal(spi.readbytes(3))) 
-		#time.sleep(0.05)
-		#GPIO.output(board,GPIO.HIGH)
-		#time.sleep(0.05)
-
-		#Coefficients for Chebyshev polynomials - convert resistance/voltage to temp.
-		#Coef. resistors (Resistance)
-		r = [31.3231717,2.20213448,1.66482204e-3,-3.98620116e-6,4.74888650e-9,
-			7.27263261e-12,-2.80325700e-14,3.11300149e-17,-1.22123964e-20] 
- 		#Coef. diodes (Voltage)
-		d = [2.06215201e4,-3.72240694e4,2.81733106e4,-1.78708069e4,9.20412215e3,
-			-3.73253855e3,1.12440290e3,-2.25464342e2,22.4784626]
-
-		print("Sensor %s --------" %sensor)
-		time.sleep(0.05)
-		while True:
-			#Read Status register: check that DOUT/RDY is low for conversion
-			GPIO.output(board,GPIO.LOW)
-			spi.writebytes(bytes(read|0x00,8))
-			status = decimal(spi.readbytes(1))
-			time.sleep(0.05)
-			GPIO.output(board,GPIO.HIGH)
-			time.sleep(0.05)
-
-			if status == 0x40:
-				print("LP ADC error - checking error register")
-				GPIO.output(board,GPIO.LOW)
-				time.sleep(0.05)
-				spi.writebytes(bytes(read|0x06,24))
-				error = decimal(spi.readbytes(3))
-				time.sleep(0.05)
-				GPIO.output(board,GPIO.HIGH)
-				print("Error register: %s" %hex(error))
-				output = 11111
-				
-				while status == 0x40: #Why isn't the error bit in the status register not clearing after reading the error register??
-					GPIO.output(board,GPIO.LOW)
-					spi.writebytes(bytes(read|0x00,8))
-					status = decimal(spi.readbytes(1))
-					time.sleep(0.05)
-					GPIO.output(board,GPIO.HIGH)
-					time.sleep(0.05)
-					print(hex(status))
-				break
-
-			elif status == 0x3:
-				print("Sensor %s - wrong channel error" %sensor)
-				output = 11111
-				break
-
-			#print(status)
-			#If DOUT/RDY == low, then read conversion from the Data register:
-			#if 0x0 <= status and 0x0F >= status:
-			if status == 0x0:
-				time.sleep(0.05)
-				GPIO.output(board,GPIO.LOW)
-				spi.writebytes(bytes(read|0x02,8))
-				raw = spi.readbytes(3)
-				print(raw)
-				data = decimal(raw)
-				time.sleep(0.05)
-				GPIO.output(board,GPIO.HIGH)
-
-				polyR = np.polynomial.Chebyshev(r)
-				polyD = np.polynomial.Chebyshev(d)
-
-				if sensor in diodes:
-					V = data*2.5175/(2**(24)) #24bits to voltage w/ 3.3V reference
-					#V = data*3.3/(2**(24))
-					time.sleep(0.05)
-					print(data)
-					#V = V*0.5647/s[sensor][2]
-					#V = V*0.5664/s[sensor][2]
-
-					if data > 16700000:
-						output = 11111
-						break
-
-					output = polyD(V)-273.15
-					#print(output)
-
-				elif sensor in resistors:
-					R = data*2.5175/(2**(24)*50e-6) #24bits to resistance w/ 3.3V reference and 50microA excitation current
-					#R = data*3.3/(2**(24)*50e-6)
-					time.sleep(0.05)
-					print(data)
-					#print(R)
-
-					#R = R*109.605/s[sensor][2]
-					###R = R*109.315/s[sensor][2]
-
-					if data > 16700000:
-						output = 11111
-						break	
-			
-
-					output = polyR(R)-273.15
-					#print(output)
-				break
-		#output = data
-		print(output)
-		return output
 	
